ece15dc5c28b32db6896c0bf6209a3b52d8bb1e6
蒋晓翠 <1765373189@qq.com>
2021-02-03 16:09:35 +0800

    第二周

---
+++ week2/src/Task2.java
@@ -0,0 +1,55 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;//字符串数组个数
+        System.out.println("想输入字符串数组的个数:");
+        n = in.nextInt();
+        String[] strings = new String[n];
+        System.out.println("输入字符串数组元素：");
+        for (int i = 0; i < n; i++)
+        {
+            strings[i] = in.next();
+        }
+        String pre=GetCommonString(strings);
+        System.out.println("共同前缀为："+pre);
+    }
+    //寻找相同前缀
+    public static String GetCommonString(String[] strings)
+        {
+            String s = "";
+            //先找出最短的字符串
+            String mins=getMinString(strings);
+            //将最短字符串与其他字符串比较
+            for(int i =0;i< mins.length();i++)
+            {
+                char a=mins.charAt(i);
+                //只要有一个字符串第一个字母与其他字符串不相等时，就没有共同前缀
+                for (int j=1;j<strings.length;j++)
+                {
+                    if(a!=strings[j].charAt(i))
+                        return s;
+                }
+                s+=a;
+            }
+            return s;
+        }
+     //找出字符串数组数组中最短的字符串
+    public static String getMinString(String[] strings)
+    {
+        String mins=strings[0];//最短字符串
+        int min = strings[0].length();//最短字符串的长度
+        for (int i = 1; i < strings.length; i++)
+        {
+            if (min > strings[i].length())
+            {
+                mins = strings[i];
+            }
+        }
+        return mins;
+    }
+}
+
