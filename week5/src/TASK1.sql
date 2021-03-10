f98d8f66ac7e05611b0a48c37607ed001952ddbc
蒋晓翠 <1765373189@qq.com>
2021-03-10 22:57:43 +0800

    第五周

---
+++ week5/src/TASK1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE cqupt_student(
+                              studentid VARCHAR(10),
+                              NAME VARCHAR(20),
+                              sex INTEGER,
+                              Fee DECIMAL(10,2),
+                              address VARCHAR(50),
+                              memo VARCHAR(300)
+);
+SHOW TABLES;
+DESC cqupt_student;
